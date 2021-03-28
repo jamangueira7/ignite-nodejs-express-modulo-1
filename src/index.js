5f582f79cb648acd8512b290f8a19aa672e97c2d
João Mangueira <jamjs7@gmail.com>
2021-03-28 16:54:15 -0300

    add criação de conta

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
 
 app.get('/', (req, res) => {
-    return res.send('Hello world!');
+    return res.json({ message: 'Hello world Ignite!' });
+})
+
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return res.status(201).send();
+
+
 })
 
 app.listen(3333);
